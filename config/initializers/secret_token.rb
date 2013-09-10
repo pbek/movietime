2a81699be07fe93f197c8619d8f369f7b730d539
Patrizio Bekerle <patrizio@bekerle.com>
2013-09-10 07:47:32 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movietime::Application.config.secret_token = '05e91ebc88a70caf21a5a73708796caeb777fc5d5996cdeecd8a7985b5ed57e622f033752aa940c7e35e3670bfdcd0e617c576cce355306e52d3bf8a230c6f9c'
