ad3b850350a5b6c797fd562825f7be595559b257
Patrizio Bekerle <patrizio@bekerle.com>
2013-09-11 16:41:59 +0200

    implemented movie scan terminal

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,63 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :imdb_id, :directory_name, :source_id, :length, :name, :plot, :plot_summary, :plot_synopsis, :poster_url, :rating, :tagline, :trailer_url, :votes, :year
+  attr_accessible :imdb_id, :directory_name, :source_id, :length, :name, :plot, :plot_summary, :plot_synopsis
+  attr_accessible :poster_url, :rating, :tagline, :trailer_url, :votes, :year
 
   belongs_to :source
 
   validates :name,
     :presence => true
+
+  def self.cleanupDirectoryName(str)
+    replacements = [
+      [/DVDRip/i, ""],
+      [/XviD/i, ""],
+      [/WRD/, ""],
+      [/UPA/, ""],
+      [/morgie/, ""],
+      [/aXXo/, ""],
+      [/DiAMOND/, ""],
+      [/AXED/, ""],
+      [/BRRip/i, ""],
+      [/BDRip/i, ""],
+      [/SCREAM/, ""],
+      [/720\w/i, ""],
+      [/1080\w/i, ""],
+      [/torrents/i, ""],
+      [/CD/i, ""],
+      [/DVD/i, ""],
+      [/MP3/i, ""],
+      [/HDSi/i, ""],
+      [/ Rip/i, ""],
+      [/x264/i, ""],
+      [/AC3/i, ""],
+      [/HDRiP/i, ""],
+      [/subtitle\w*/i, ""],
+      [/-[\w]+$/, ""],
+      [/-/, " "],
+      [/\./, " "],
+      [/\[.*\]/, ""],
+      [/AC3-ViSiON/i, ""],
+    ]
+    result = str.clone
+  	replacements.each {|replacement| result.gsub!(replacement[0], replacement[1])}
+
+  	result.squeeze(' ').strip
+  end
+
+  def self.create_from_imdb(i)
+    movie = Movie.new
+    movie.imdb_id = i.id
+    movie.length = i.length
+    movie.name = i.title
+    movie.plot = i.plot
+    movie.plot_summary = i.plot_summary
+    movie.plot_synopsis = i.plot_synopsis
+    movie.poster_url = i.poster
+    movie.rating = i.rating
+    movie.tagline = i.tagline
+    movie.trailer_url = i.trailer_url
+    movie.votes = i.votes
+    movie.year = i.year
+    movie
+  end
 end
--- lib/tasks/movies.rake
+++ lib/tasks/movies.rake
@@ -11,10 +11,38 @@ namespace :movies do
       exit 1
     end
 
+    # clear the screen
+    puts "\e[H\e[2J"
     puts "Searching for movies..."
 
     sources.each do |source|
-      puts "  Scanning directory '#{source.path}' for source '#{source.name}'"
+      puts "Scanning directory '#{source.path}' for source '#{source.name}'"
+      directories = Dir.entries(source.path).select {|entry| File.directory? File.join(source.path,entry) and !(entry =='.' || entry == '..') }
+      directories.each do |directory|
+        next unless Movie.find_by_directory_name_and_source_id(directory, source.id).nil?
+
+        puts "- found new directory '#{directory}"
+        clean_name = Movie::cleanupDirectoryName(directory)
+        puts "  - make your movie choice for '#{clean_name}' or enter imdb id (skip with '-')"
+
+        i = Imdb::Search.new(clean_name)
+        imdb_movies = i.movies
+        imdb_movies[0..9].each_with_index do |imdb_movie, key|
+          puts "    (#{key}): #{imdb_movie.title}"
+        end
+
+        choice = STDIN.gets.chomp
+        next if choice == "-"
+
+        imdb_movie = (choice.size > 1) ? Imdb::Movie.new(choice) : imdb_movies[choice.to_i]
+        next if imdb_movie.nil? || imdb_movie.title.nil?
+
+        puts "  - your choice: #{imdb_movie.title}"
+        movie = Movie::create_from_imdb(imdb_movie)
+        movie.directory_name = directory
+        movie.source = source
+        movie.save
+      end
     end
   end
 end
