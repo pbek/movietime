bb11176bc8420164750e1151ebd69062e5008384
Patrizio Bekerle <patrizio@bekerle.com>
2013-09-16 19:26:49 +0200

    DirectoryNameCleanupPattern fixed

--- app/models/directory_name_cleanup_pattern.rb
+++ app/models/directory_name_cleanup_pattern.rb
@@ -4,19 +4,18 @@ class DirectoryNameCleanupPattern < ActiveRecord::Base
   default_scope order('priority DESC')
 
   def search
-  	self.pattern[0] == '/' ? eval(self.pattern) : self.pattern
+    # eval: unsafe but ok here, because the app is self hosted
+    self.pattern[0] == '/' ? Regexp.new(eval(self.pattern)) : self.pattern
   end
 
   def gsub!(str)
-  	puts self.pattern
-  	puts self.search
-  	str.gsub!(self.search, self.replace)
+    str.gsub!(self.search, self.replace)
+    return str
   end
 
-  def self.clean(str)
+  def self.cleanup(str)
   	result = str.clone
-  	puts result
-  	DirectoryNameCleanupPattern.all.each {|cleanup_pattern| puts result = cleanup_pattern.gsub!(result) unless result.nil?}
+  	DirectoryNameCleanupPattern.all.each {|cleanup_pattern| result = cleanup_pattern.gsub!(result) unless result.nil?}
   	result.squeeze(' ').strip unless result.nil?
   end
 end
